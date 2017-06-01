1591c83f6f7a050dd33f5038d0e31d808873eade
CLOWN-S <704093985@qq.com>
2017-06-01 18:29:50 +0800

    loading

--- getPics.php
+++ getPics.php
@@ -1,8 +1,7 @@
 ﻿<?php
 header('Content-type:text/html; charset="utf-8"');
-$cpage = isset($_GET['cpage']) ? $_GET['cpage'] : 1;
-$url = 'http://v.juhe.cn/toutiao/index?type=top&key=5c8f110a97dca3537dd1a1a6850441b5';
+$url='http://v.juhe.cn/toutiao/index?type=top&key=5c8f110a97dca3537dd1a1a6850441b5';
 $content = file_get_contents($url);
-$content = iconv('gbk', 'utf-8', $content);
+//$content = iconv('gbk', 'utf-8', $content);
 echo $content;
 ?>
