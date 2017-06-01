3b1cbb3a92b19d0ffc500edb18d37d79a624b3ae
CLOWN-S <704093985@qq.com>
2017-06-01 12:04:02 +0800

    shouye

---
+++ denglu.js
@@ -0,0 +1,16 @@
+var btn=document.getElementsByClassName('btn')[0];
+ var ipts=document.querySelector('input[type="text"]'),
+		    pds=document.querySelector('input[type="password"]');
+		  var ooo=localStorage.getItem('ipts'),
+		     ppp=localStorage.getItem('pds');
+
+    btn.onclick=function(){
+		    	if((ipts.value==ooo)&&(pds.value==ppp)){
+		    		console.log(ipts.value=="ooo",pds.value=="ppp")
+		    		alert('登陆成功')
+		    	}else{
+		    		alert('账号或密码错误')
+		    	}
+		    	
+		    	
+		    }
---
+++ zhuce.js
@@ -0,0 +1,23 @@
+var btn=document.querySelector('div[class="btn"]');
+ var ipts=document.querySelector('input[type="text"]'),
+		    pds=document.querySelector('input[type="password"]');
+		    var zhuye=document.getElementById('zhuye');
+		   var str1=/[0-9]{6,12}/;
+		  var  str2= /^[a-zA-Z]\w{5,17}$/;
+			 btn.onclick=function(){
+		  	if((str1.test(ipts.value))&&(str2.test(pds.value))){
+		  		  localStorage.setItem('ipts',ipts.value);
+          		  localStorage.setItem('pds',pds.value);
+          		  alert('注册成功')
+          		  window.open('登录.html')
+		  	}else{
+		  		alert('您输入有误')
+		  	}
+		
+		  };
+		  zhuye.onclick=function(){
+		  	window.open('登录.html')
+		  }
+		  
+		  
+	
