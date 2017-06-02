9e97f4e78f150c5689e56f44bbc3f0ef95f18d65
CLOWN-S <704093985@qq.com>
2017-06-02 18:11:33 +0800

    max

---
+++ getPics1.php
@@ -0,0 +1,6 @@
+﻿<?php
+header('Content-type:text/html; charset="utf-8"');
+$url = 'http://www.wookmark.com/api/json/popular?page=1';
+$content = file_get_contents($url);
+echo $content;
+?>
--- js/xinlang.js
+++ js/xinlang.js
@@ -53,16 +53,20 @@ $.ajax({
     type:'GET',
     success:function (res) {
         var data = JSON.parse(res);
-        console.log(data.result.data)
+        //console.log(data.result.data)
         let resultData = data.result.data;
         var str = '';
         var stb = '';
         resultData.forEach(function (item,index) {
 		//$.each(data,function (item,index) {
-		for(var j=10;j<footerbox1.length;j++){
-			stb += `<div class="swiper-slide"><img  src="${item.thumbnail_pic_s}" /><p style="text-shadow:1px 1px 2px #000000;">${item.title}</p></div>`;
-		}
-    	str += `<div class="footer-box"><img class="footer-img" src="${item.thumbnail_pic_s}" /><p class="footer-text">${item.title}</p><i class="icon-huihua1 iconfont"></i><span class="footer-text2"></span></div><hr />`;
+			//for(var j=10;j<footerbox1.length;j++){
+			if(index>=9){
+				//console.log(index,stb)
+				stb += `<div class="swiper-slide"><img  src="${item.thumbnail_pic_s}" /><p style="text-shadow:1px 1px 2px #000000;">${item.title}</p></div>`;
+				
+			}
+			//}
+	    	str += `<div class="footer-box"><img class="footer-img" src="${item.thumbnail_pic_s}" /><p class="footer-text">${item.title}</p><i class="icon-huihua1 iconfont"></i><span class="footer-text2"></span></div><hr />`;
     	
 		})
         astb.innerHTML = stb;
@@ -72,21 +76,23 @@ $.ajax({
 		ap[i].index=i;
 		var text = Math.floor(Math.random()*500);
 		ap[i].textContent=text;
+		}
 		//轮播图
 		var mySwiper = new Swiper ('.swiper-container', {
 	   	loop: true,
 	    pagination: '.swiper-pagination',
 	    nextButton: '.swiper-button-next',
 	    prevButton: '.swiper-button-prev',
-	    autoplay:5000,
+	    autoplay:2000,
 	    onSlideChangeEnd: function(swiper){
-	  	 swipertext1.textContent=swiper.activeIndex
+	    	//alert(swiper.activeIndex)
+	  		swipertext1.textContent=swiper.activeIndex
+	  		swiper.startAutoplay();
 	    }
 	  	})
 	  	//数字设置
 	  	swipertext2.textContent="/"+footerbox1.length;
-	  	
-}
+//}
 }    
 })
 window.onload = function() {
